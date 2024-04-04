498c341d8b8b69fa6d9cf242a80c5df9f5e6925a
unknown <andredrummersullivan@gmail.com>
2024-04-04 14:38:41 -0300

    Commitando mudanças antes de mudar para a branch main

---
+++ CalculadoraSoma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fr {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bem-vindo ao programa de soma de números!");
+        
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextInt();
+        
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextInt();
+        
+        double soma = num1 + num2;
+        
+        System.out.println("A soma de " + num1 + " + " + num2 + " é igual a: " + soma);
+        
+        scanner.close();
+    }
+}
